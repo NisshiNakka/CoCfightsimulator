fd03d90ba2c931791f671fdc1a9d21b8e9c51711
NisshiNakka <ryouta.nakka@gmail.com>
2026-01-07 18:14:07 +0900

    feature: [#18] キャラクター登録ページを実装しました。

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,4 +2,12 @@ class CharactersController < ApplicationController
   def index
     @characters = current_user.characters.order(created_at: :desc).page(params[:page])
   end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+    #
+  end
 end
