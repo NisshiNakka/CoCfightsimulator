b1edea2b42743a2c04f2373fab9c55c9847b3d16
NisshiNakka <ryouta.nakka@gmail.com>
2026-01-07 19:09:43 +0900

    feature: [#18] キャラクターを実際に登録できるようcreateアクションを設定しました。

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -8,6 +8,18 @@ class CharactersController < ApplicationController
   end
 
   def create
-    #
+    @character = current_user.characters.build(character_params)
+    if @character.save
+      redirect_to characters_path, success: t("defaults.flash_message.created", item: Character.model_name.human)
+    else
+      flash.now[:danger] = t("defaults.flash_message.not_created", item: Character.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :hitpoint, :dexterity, :evasion_rate, :evasion_correction, :armor, :damage_bonus)
   end
 end
