8f01a11f9d11cd75a37e45a1372af261420afd82
NisshiNakka <ryouta.nakka@gmail.com>
2026-03-29 22:08:56 +0900

    feature: [#86] 独自ドメインを適用するための設定を行いました。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "cocfightsimulator.com"      # 独自ドメイン
+  config.hosts << "www.cocfightsimulator.com"  # サブドメイン
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
